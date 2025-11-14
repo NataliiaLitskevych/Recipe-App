1286c7b95fb443a7aca494999799a3a9afe28517
Nataliia Litskevych <nataliia.litskevych@gmail.com>
2025-11-14 01:00:26 -0500

    Added all the logic for searching and submitting recipes to component

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,33 @@ import MyRecipesComponent from './MyRecipesComponent';
 
 function App() {
 
-  //https://api.edamam.com/api/recipes/v2?type=public&q=lemon&app_id=b7edc033&app_key=4dfb2226902f5955cc49e865c117217f
+  //https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=b7edc033&app_key=4dfb2226902f5955cc49e865c117217f
 
   const MY_ID="b7edc033";
   const MY_KEY="4dfb2226902f5955cc49e865c117217f";
 
   const [mySearch, setMySearch] = useState("");
   const [myRecipes, setMyRecipes] = useState([]);
+  const [wordSubmitted, setWordSubmitted] = useState("avocado")
 
   useEffect(() => {
     const getRecipe = async () => {
-    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=lemon&app_id=${MY_ID}&app_key=${MY_KEY}`);
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
     const data = await response.json();
     console.log(data.hits); 
     setMyRecipes(data.hits); 
   }
     getRecipe()
-  }, [])
+  }, [wordSubmitted])
 
   const myRecipeSearch = (e) => {
     console.log(e.target.value)
     setMySearch(e.target.value)
+  }
+
+  const finalSearch = (e) => {
+    e.preventDefailt();
+    setWordSubmitted(mySearch);
   }
 
   return (
@@ -38,19 +44,23 @@ function App() {
       </div>
 
       <div className='container'>
-        <form>
+        <form onSubmit={finalSearch}>
           <input className='search' placeholder='Search...' onChange={myRecipeSearch} value={mySearch}/>
         </form>
       </div>
 
       <div className='container'>
-        <button>
+        <button onClick={finalSearch}>
           <img src="https://img.icons8.com/fluency/48/000000/fry.png" alt="icon" />
         </button>
       </div>
 
-      {myRecipes.map(element => (
-        <MyRecipesComponent label={element.recipe.label} image={element.recipe.image}/>
+      {myRecipes.map((element, index) => (
+        <MyRecipesComponent key={index}
+        label={element.recipe.label} 
+        image={element.recipe.image} 
+        calories={element.recipe.calories}
+        ingredients={element.recipe.ingredientLines}/>
       ))}
     
 
